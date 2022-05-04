3f494d7ba08a4591fd6a5e303a01e066efba99e4
qinwen <54129317@qq.com>
2022-05-04 17:26:09 +0800

    修改了pointCloud相关代码

--- Assets/Resources/MyScript/DynamicPC/PointCloudController.cs
+++ Assets/Resources/MyScript/DynamicPC/PointCloudController.cs
@@ -7,11 +7,19 @@ public class PointCloudController : MonoBehaviour
 
     public string hostIP_ar;
     public string hostIP_vr;
+    public string hostIP;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (GlobleInfo.ClientMode.Equals(CameraMode.VR))
+        {
+            hostIP = hostIP_vr;
+        }
+        else
+        {
+            hostIP = hostIP_ar;
+        }
     }
 
     // Update is called once per frame
